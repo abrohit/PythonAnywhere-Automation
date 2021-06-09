3cd8eacf45b87a06c8336fbb700d74368b5bfef2
abrohit <abrohit05@gmail.com>
2021-06-09 12:15:17 +0530

    code cleaning

--- main.py
+++ main.py
@@ -5,33 +5,69 @@ from selenium.webdriver.chrome.options import Options
 
 import time
 
-def webapp():
+class Automate():
 
-    URL = 'https://www.pythonanywhere.com/login/'
+    def __init__(self, email: str, password: str):
 
-    config = dotenv_values(".env")
+        self.BASEURL = ''
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument('disable-notifications')
-    options.add_argument('--disable-infobars')
-    self.options.add_argument('--headless')
 
-    driver = webdriver.Chrome('.chromedriver.exe', options = options)
+        self.options = Options()
+        self.options.add_argument("--start-maximized")
+        self.options.add_argument('disable-notifications')
+        self.options.add_argument('--disable-infobars')
+        self.options.add_argument('--headless')
 
-    driver.get(URL)
+        self.driver = webdriver.Chrome('chromedriver.exe', options = self.options)
 
-    driver.find_element_by_name('auth-username').send_keys(config['email'])
-    time.sleep(0.5)
-    driver.find_element_by_name('auth-password').send_keys(config['pass'])
+        self.EMAIL = email
+        self.PASS = password
 
-    driver.find_element_by_id('id_next').click()
+    #Logs the user in.
+    def login(self):
 
-    time.sleep(1)
+        self.driver.get('https://www.pythonanywhere.com/login/')
 
-    driver.get('https://www.pythonanywhere.com/user/abrohit/webapps/')
-    time.sleep(0.5)
-    driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div[6]/div/div/div/form/input[2]").click()
+        self.driver.find_element_by_name('auth-username').send_keys(self.EMAIL)#Enters Email.
+        time.sleep(0.5)
+        self.driver.find_element_by_name('auth-password').send_keys(self.PASS)#Enters Password.
+
+        self.driver.find_element_by_id('id_next').click()#Clicks on login button.
+
+        time.sleep(1)
+
+        self.BASEURL = self.driver.current_url#Gets URL once logged in.
+        
+    #Runs Webapp forever.
+    def webapp(self):
+
+        self.driver.get(self.BASEURL + 'webapps/')#Once logged in driver loads webapp page.
+        time.sleep(0.5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div/div[2]/div/div/div[6]/div/div/div/form/input[2]").click()#Clicks on 'Run for 3 months' button.
+
+    #Runs the task forever.
+    def tasks(self):
+
+        self.driver.get(self.BASEURL + 'tasks_tab/')#Once logged in driver loads tasks page.
+        time.sleep(0.5)
+        self.driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[2]/div[3]/div/div/table/tbody/tr/td[5]/button[4]").click()#Clicks on 'Extend Expiry' button.
+
+    #Runs the functions.
+    def run(self):
+
+        self.login()
+        self.webapp()
+        self.tasks()
+
+    #Quits the driver.
+    def quit(self):
+        self.driver.quit()
     
 if __name__ == '__main__':
-    webapp()
+
+    config = dotenv_values(".env")
+    
+    webapp = Automate(config['email'], config['pass'])
+    webapp.run()
+    webapp.quit()
+    
